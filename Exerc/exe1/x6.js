fd0414741ea8867ed7b650e4531c990c7dc74827
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-09-26 19:05:03 -0300

    ex6

---
+++ Exerc/exe1/x6.js
@@ -0,0 +1,27 @@
+function iniciar() {
+
+  // utiliza a condição ate que chegue no numero 
+  // que o usuario digitou
+    
+    alert("Bem vindo Ao site");
+    frase.innerHTML = "<h1>Clique no botão para calcular <br>";
+ }
+
+ function clicou() {
+  //// SOLICITA O NUMERO
+  var n1 = parseInt(prompt("x6_Digite um Numero"));
+  
+  var n2 = parseInt(prompt("x5_Digite um Numero"));
+
+  soma = (n1+n2);
+
+  alert(soma);
+  
+  
+  //// ESCREVE NO ARQUIVO
+  // document.write("<h1>Identificação</h1>"+"Ola " + nome );
+  // utilizando containers
+  // o nome
+
+   
+ } 
