7132533f760cbe5d7e93b435d1e981e8da65c78e
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-09-27 14:00:04 -0300

    ff

--- formulario.js
+++ formulario.js
@@ -5,3 +5,15 @@ function iniciar() {
 function somar() {
   res.value = parseInt(v1.value) + parseInt(v2.value); 
 }
+
+function subt() {
+  res.value = parseInt(v1.value) - parseInt(v2.value); 
+}
+
+function mult() {
+  res.value = parseInt(v1.value) * parseInt(v2.value); 
+}
+
+function div() {
+  res.value = parseInt(v1.value) / parseInt(v2.value); 
+}
